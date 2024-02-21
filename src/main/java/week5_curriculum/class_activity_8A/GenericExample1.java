0732ae4d37e5d1a15e9bfe884df9c903b233fdf4
Isioma Nwobi <stellanwobs@gmail.com>
2024-02-21 12:09:45 +0100

    commit files

--- src/main/java/week5_curriculum/class_activity_8A/GenericExample1.java
+++ src/main/java/week5_curriculum/class_activity_8A/GenericExample1.java
@@ -1,4 +1,15 @@
 package week5_curriculum.class_activity_8A;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericExample1 {
+    public static void main(String[] args) {
+        List list = new ArrayList<>();
+
+        list.add(10);
+        list.add("10");
+
+        System.out.println(list);
+    }
 }
---
+++ src/main/java/week5_curriculum/class_activity_9A/GenericWithMethod.java
@@ -0,0 +1,8 @@
+package week5_curriculum.class_activity_9A;
+
+public class GenericWithMethod {
+    public static <E> void printArray(E[] elements) {
+
+
+    }
+}
